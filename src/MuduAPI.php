4344d4b0c6728446e3a56b7b250f78ec8273a9ad
Jack Yang <ibca@163.com>
2019-04-12 17:10:27 +0800

    feat:request post body

--- src/MuduAPI.php
+++ src/MuduAPI.php
@@ -27,9 +27,10 @@ class MuduAPI
     public function __construct($access_token)
     {
         $this->http_header = array(
-            'Authorization:Bearer ' . $access_token,
-            'Content-Type:application/json'
+            "Authorization" => "Bearer ". $access_token,
+            "Content-Type" => "application/json"
         );
+        $this->client = new Client(['base_uri' => config('mudu.api_url'),'headers' => $this->http_header]);
     }
 
     /**
@@ -48,12 +49,15 @@ class MuduAPI
         $options = array();
         if ($live !== '') {
             $options = array_merge(['live' => $live], $options);
-        }else if ($manager !== '') {
+        } else if ($manager !== '') {
             $options = array_merge(['manager' => $manager], $options);
-        }else if ($p !== ''){
+        } else if ($p !== ''){
             $options = array_merge(['p' => $p], $options);
         }
-        return $this->get(config('mudu.api_url') . $api , $options);
+        $response = $this->client->request('GET', $api,[
+            'query' => $options
+        ]);
+        return  json_decode($response->getBody()->getContents());
     }
 
     /**
@@ -62,12 +66,9 @@ class MuduAPI
      * @return $result [返回array数组对象]
      */
     public function getActivity($id){
-        $api = '/v1/activities';
-        $options = array();
-        if ($id !== '') {
-            $options = array_merge(['id' => $id], $options);
-        }
-        return $this->get(config('mudu.api_url') . $api , $options);
+        $api = '/v1/activities/'.$id;
+        $response = $this->client->request('GET', $api);
+        return  json_decode($response->getBody()->getContents());
     }
 
     /**
@@ -79,16 +80,27 @@ class MuduAPI
      * @return $result [返回array数组对象]
      */
     public function createActivity($name = '',$start_time = '',$act_manager_id = ''){
+
         $api = '/v1/activities';
         $options = array();
+        if ($act_manager_id !== ''){
+            $options = array_merge(['act_manager_id' => $act_manager_id], $options);
+        } else {
+            return ['success' => false , 'message' => '频道名称必填'];
+        }
+
+        if ($start_time !== '') {
+            $options = array_merge(['start_time' => $start_time], $options);
+        }
         if ($name !== '') {
             $options = array_merge(['name' => $name], $options);
-        }else if ($start_time !== '') {
-            $options = array_merge(['start_time' => $start_time], $options);
-        }else if ($act_manager_id !== ''){
-            $options = array_merge(['act_manager_id' => $act_manager_id], $options);
         }
-        return $this->post(config('mudu.api_url') . $api , $options);
+
+        $response = $this->client->request('POST', $api, [
+            'body' => json_encode($options)
+        ]);
+
+        return  json_decode($response->getBody()->getContents());
     }
 
     /**
@@ -97,16 +109,14 @@ class MuduAPI
      * @return $result [返回array数组对象]
      */
     public function getActivityReport($id = ''){
-        if ($id !== '') {
-            $api = '/v1/activities/'.$id.'/report';
-            return $this->get(config('mudu.api_url') . $api);
-        }else{
-            return [ 'success' => false,'errorMessage' => '频道ID不能为空'];
-        }
+        $api = '/v1/activities/' . $id . '/report';
+        $response = $this->client->request('GET', $api);
+        return  json_decode($response->getBody()->getContents());
     }
 
     /**
      * 创建管理员
+     * 仅企业版可用
      * api: /v1/account/createActManager
      * username	管理员用户名	string	是
      * cost_type	消费上限类型	integer	是	1表示时长限制，2表示流量限制
@@ -115,19 +125,20 @@ class MuduAPI
      */
     public function createManager($username = '' ,$cost_type = '', $cost_limit = ''){
         $api = '/v1/account/createActManager';
-        $options = array();
-        if ($username !== '') {
-            $options = array_merge(['username' => $username], $options);
-        }else if ($cost_type !== '') {
-            $options = array_merge(['cost_type' => $cost_type], $options);
-        }else if ($cost_limit !== ''){
-            $options = array_merge(['cost_limit' => $cost_limit], $options);
+        if ($username === '' || $cost_type === '' || $cost_limit === '' ) {
+            return ['success' => false , 'message' => '管理员用户名,消费上限类型,消费上限量:必填'];
         }
-        return $this->put(config('mudu.api_url') . $api , $options);
+        $options = array('username' => $username, 'cost_type' => $cost_type ,'cost_limit' => $cost_limit);
+        $response = $this->client->request('PUT', $api, [
+            'body' => json_encode($options)
+        ]);
+        return  json_decode($response->getBody()->getContents());
+
     }
 
     /**
      * 删除管理员
+     * 仅企业版可用
      * api: /v1/account/deleteActManager
      * manager_id	要删除的管理员ID	integer	是
      * is_delete_act	是否删除该管理员创建的频道	integer	是	1表示删除，0表示不删除
@@ -135,111 +146,14 @@ class MuduAPI
      */
     public function deleteManager($manager_id = '', $is_delete_act = ''){
         $api = '/v1/account/deleteActManager';
-        $options = array();
-        if ($manager_id !== '') {
-            $options = array_merge(['manager_id' => $manager_id], $options);
-        }else if ($is_delete_act !== '') {
-            $options = array_merge(['is_delete_act' => is_delete_act], $options);
+        if ($manager_id === '' || $is_delete_act === '') {
+            return ['success' => false , 'message' => '要删除的管理员ID,是否删除该管理员创建的频道:必填'];
         }
-        return $this->delete(config('mudu.api_url') . $api , $options);
-    }
-
-
-
-    /**
-     * Return GuzzleHttp\Client instance.
-     *
-     * @return \GuzzleHttp\Client
-     */
-    public function getClient()
-    {
-        if (!($this->client instanceof Client)) {
-            $this->client = new Client(['headers' => $this->http_header]);
-        }
-
-        return $this->client;
-    }
-
-    /**
-     * GET request.
-     *
-     * @param string $url
-     * @param array $options
-     *
-     * @return ResponseInterface
-     *
-     * @throws HttpException
-     */
-    public function get($url, array $options = [])
-    {
-        return $this->request('GET', $url, ['query' => $options]);
-    }
-
-    /**
-     * POST request.
-     *
-     * @param string $url
-     * @param array $form
-     *
-     * @return ResponseInterface
-     */
-    public function post($url, array $form = [])
-    {
-        return $this->request('POST', $url, ['form_params' => $form]);
-    }
-
-    /**
-     * PUT request.
-     *
-     * @param string $url
-     * @param array body
-     *
-     * @return ResponseInterface
-     */
-    public function put($url, array $body = [])
-    {
-        return $this->request('PUT', $url, ['body' => $body]);
-    }
-
-    /**
-     * DELETE request.
-     *
-     * @param string $url
-     * @param array $json
-     *
-     * @return ResponseInterface
-     */
-    public function delete($url, array $json = [])
-    {
-        return $this->request('DELETE', $url, ['json' => json_encode($json)]);
-    }
-
-
-    /**
-     * Make a request.
-     *
-     * @param string $url
-     * @param string $method
-     * @param array $options
-     *
-     * @return ResponseInterface
-     */
-    public function request($method, $url, $options = [])
-    {
-        $method = strtoupper($method);
-
-        Log::debug('Client Request:', compact('url', 'method', 'options'));
-
-        $response = $this->getClient()->request($method, $url,$options);
-
-        Log::debug('API response:', [
-            'Status' => $response->getStatusCode(),
-            'Reason' => $response->getReasonPhrase(),
-            'Headers' => $response->getHeaders(),
-            'Body' => strval($response->getBody()),
+        $options = array('$manager_id' => $manager_id, '$is_delete_act' => $is_delete_act);
+        $response = $this->client->request('DELETE', $api, [
+            'body' => json_encode($options)
         ]);
-
-        return json_decode($response->getBody()->getContents(), true);
+        return  json_decode($response->getBody()->getContents());
     }
 
 
